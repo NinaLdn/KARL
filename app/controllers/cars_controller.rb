2d9357b67bf67837cf85b9f6fe55a993754a53b7
Nina Ledun <n.ledun@gmail.com>
2018-12-03 18:25:59 +0100

    cars controller and the first views: home/new/create, first_estimation, start, edit

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,25 +1,68 @@
 class CarsController < ApplicationController
-
+  before_action :set_car, only: [:show, :edit, :first_estimation, :start]
   def index
+    # FOR ALL CARS OF 1 USER
+    @cars = Booking.all
   end
 
   def show
+    # FOR THE TECHNICAL DATA SEEH OF 1 CAR
+    # @car = Car.find(params[:id])
   end
 
   def new
+    # USER STORY 1: HOME PAGE
+    @car = Car.new
   end
 
   def create
-
+    # USER STORY 1: HOME PAGE
+    @car = Car.new(car_params)
+    @car.user = current_user
+    if @car.save
+      redirect_to first_estimation_car_path(@car)
+    else
+      render :new
+    end
   end
 
   def edit
+    # USER STORY 4 et 5: Edit info complementaires
+    # INFO COMPLEMENTAIRES SUR PLUSIEURS PAGES (TO SAVE A CHAQUE ETAPE DE L'EDIT): KILOMETRAGE EXACT / PICTURES / WHY_SELLING / VALIDATION DE L ANNONCE
   end
 
   def update
+    # USER STORY 4 et 5: Edit info complementaires
+    # INFO COMPLEMENTAIRES SUR PLUSIEURS PAGES (TO SAVE A CHAQUE ETAPE DE L'EDIT): KILOMETRAGE EXACT / PICTURES / WHY_SELLING / VALIDATION DE L ANNONCE
   end
 
   def destroy
+  end
+
+  def first_estimation
+    # USER STORY 2
+    # @car = Car.find(params[:id])
+    # pricing API Argus pour estimation du prix
+    # redirect_to start page
+  end
+
+  def start
+    # USER STORY 3
+    # @car = Car.find(params[:id])
+  end
+
+  def final_message
+    # USER STORY 6: MESSAGE DE VALIDATION QUE L'ANNONCE EST BIEN POSTEE
+  end
+
+private
+
+  def set_car
+    @car = Car.find(params[:id])
+  end
+
+  def car_params
+    params.require(:car).permit(:registration_number, :estimated_kilometers)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :cars
+  controller :pages do
+    # get 'start'
+  end
+  resources :cars do
+    member do
+      get 'first_estimation', to: "cars#first_estimation"                      # collection => no restaurant id in URL
+      get 'start', to: "cars#start"  # RestaurantsController#top
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
