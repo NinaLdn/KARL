83356a56e1bc5e602b3750f258564d64a99eb54d
mathieu bouchant <matbcht@gmail.com>
2018-12-05 18:32:33 +0100

    navbar done

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2018_12_05_144639) do
-<<<<<<< HEAD
-=======
-
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
