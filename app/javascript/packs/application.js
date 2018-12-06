e829c19a2a749e536dc9b5a23edf4fd44bdf9af9
Nina Ledun <n.ledun@gmail.com>
2018-12-06 12:48:51 +0100

    car animation start

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,9 @@
 // import slider from 'shared/slider'
 import {kilometer} from 'shared/kilometer';
 import {priceChart} from 'shared/price_chart';
+import {moveCar} from 'shared/car';
 
 priceChart();
 kilometer();
+moveCar();
 
---
+++ app/javascript/shared/car.js
@@ -0,0 +1,9 @@
+// on page load, image goes from left to right
+// whem image reaches right side, repeat function
+
+const car = document.querySelector(".icon-car");
+window.onload = const moveCar = () => {
+  console.log(car);
+}
+
+export {moveCar};
