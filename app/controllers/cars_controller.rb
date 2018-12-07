1c4b9d3d932b75985a99f77f76220615424f9fe9
Nina Ledun <n.ledun@gmail.com>
2018-12-07 17:03:02 +0100

    final validation

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -146,8 +146,12 @@ class CarsController < ApplicationController
   end
 
   def dashboard
-    @car = current_user.cars.first
-    @car_price_evolution = [@car.estimated_price, @car.estimated_price*(1-@car.deval_fix), @car.estimated_price*(1-@car.deval_fix)**2,@car.estimated_price*(1-@car.deval_fix)**3, @car.estimated_price*(1-@car.deval_fix)**4]
+    @cars = current_user.cars
+    @cars.each do |car|
+      if car.estimated_price && car.deval_fix
+        @car_price_evolution = [car.estimated_price, car.estimated_price*(1-car.deval_fix), car.estimated_price*(1-car.deval_fix)**2,car.estimated_price*(1-car.deval_fix)**3, car.estimated_price*(1-car.deval_fix)**4]
+      end
+    end
   end
 
 private
