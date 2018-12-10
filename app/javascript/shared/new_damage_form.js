13d7a2dc50fa9db116a3d9442b48f1037854ec6b
Nina Ledun <n.ledun@gmail.com>
2018-12-10 19:14:03 +0100

    première étape presque fini: reprendreAlmost done: reprendre le JS pour récupérer title and description values + routes vers final validation

--- app/javascript/shared/new_damage_form.js
+++ app/javascript/shared/new_damage_form.js
@@ -1,29 +1,56 @@
 const submitDamage = () => {
 
+  const formDamage1 = document.querySelector(".formDamage1");
+  const formDamage2 = document.querySelector(".formDamage2");
+  const formDamage3 = document.querySelector(".formDamage3");
   const formDamage4 = document.querySelector(".formDamage4");
   const formDamage5 = document.querySelector(".formDamage5");
-  const formDamage6 = document.querySelector(".formDamage6");
 
   const submitYesDamage = document.getElementById("submit-yes-damage");
   const submitNoDamage = document.getElementById("submit-no-damage");
-  const submitTitleDescriptionDamage = document.getElementById("submit-damage-title-description");
+  const submitMecanique = document.getElementById("type-mecanique");
+  const submitCarrosserie = document.getElementById("type-carrosserie");
+
   const submitPicDamage = document.getElementById("submit-damage-pic");
 
   if (submitYesDamage) {
+
     submitYesDamage.addEventListener("click", (event) => {
       event.preventDefault();
-      formDamage4.classList.add("hidden")
-      formDamage5.classList.remove("hidden")
-    });
-    submitNoDamage.addEventListener("click", (event) => {
-      event.preventDefault();
+      formDamage1.classList.add("hidden")
+      formDamage2.classList.remove("hidden")
     });
 
-    submitTitleDescriptionDamage.addEventListener("click", (event) => {
+    // submitNoDamage.addEventListener("click", (event) => {
+    //   event.preventDefault();
+    // });
+
+    submitMecanique.addEventListener("click", (event) => {
       event.preventDefault();
-      formDamage5.classList.add("hidden")
-      formDamage6.classList.remove("hidden")
+      formDamage2.classList.add("hidden")
+      formDamage3.classList.remove("hidden")
     });
+
+    submitCarrosserie.addEventListener("click", (event) => {
+      event.preventDefault();
+      // console.log("hello")
+      formDamage2.classList.add("hidden")
+      formDamage4.classList.remove("hidden")
+      console.log(formDamage4)
+    });
+
+
+
+
+
+
+
+
+    // submitTitleDescriptionDamage.addEventListener("click", (event) => {
+    //   event.preventDefault();
+    //   formDamage5.classList.add("hidden")
+    //   formDamage6.classList.remove("hidden")
+    // });
   }
 };
 
--- app/models/damage.rb
+++ app/models/damage.rb
@@ -1,7 +1,8 @@
 class Damage < ApplicationRecord
+  TYPE = ["Carrosserie", "Mécanique"]
   belongs_to :car
   mount_uploader :picture_1, PhotoUploader
   mount_uploader :picture_2, PhotoUploader
   mount_uploader :picture_3, PhotoUploader
-  validates :title, presence: true
+  validates :title, presence: true, inclusion: { in: TYPE }
 end
