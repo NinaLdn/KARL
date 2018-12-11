4849dd4f448b7dfc033705e08b20234956254bd4
Emmanuel Decle <emmanuel.decle@hotmail.fr>
2018-12-11 16:22:54 +0100

    checklist

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,3 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 end
+
+def default_url_options
+  { host: ENV["HOST"] || "www.karl-app.com" }
+end
