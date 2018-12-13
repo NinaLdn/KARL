f06c4988fb59af0b33791d31b5382da83e1d356b
Emmanuel Decle <emmanuel.decle@hotmail.fr>
2018-12-13 17:16:30 +0100

    bug fixed

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,12 +1,11 @@
 class Car < ApplicationRecord
   belongs_to :user
   has_many :damages, dependent: :destroy
+
   mount_base64_uploader :photo_1, PhotoUploader
   mount_base64_uploader :photo_2, PhotoUploader
   mount_base64_uploader :photo_3, PhotoUploader
 
-
-  validates :registration_number, presence: true, :with => /^[a-zA-Z]{1,2}[0-9]{1,3}[a-zA-Z]{1,2}/, :on => create
+  validates :registration_number, presence: true, format: { multiline: true, with: /^[a-zA-Z]{1,2}[0-9]{1,3}[a-zA-Z]{1,2}/ }, on: create
   validates :estimated_kilometers, presence: true
 end
-
